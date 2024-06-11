d9bd6311c0dc384b801ee49c504066e005404819
TahaThinks <tahaa4587@gmail.com>
2024-06-11 21:11:43 +0400

    Get Property Price List

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+
+listing_url = "https://appbrewery.github.io/Zillow-Clone/"
+response = requests.get(url=listing_url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+properties = soup.find_all(name="div", attrs={'class':"StyledPropertyCardDataWrapper"})
+print(properties)
+property_price_list = []
+property_url_list = []
+property_address_list = []
+
+for property in properties:
+    property_price_list.append(property.find("span").text)
+
+# print(property_price_lists)
