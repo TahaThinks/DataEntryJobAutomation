f8179ba82a969b1f60962d13e1163e7e0f6fba95
TahaThinks <tahaa4587@gmail.com>
2024-06-11 21:21:16 +0400

    Create a Dict of the Data Gathered

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ response = requests.get(url=listing_url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 properties = soup.find_all(name="div", attrs={'class':"StyledPropertyCardDataWrapper"})
-print(properties)
+# print(properties)
 property_price_list = []
 property_url_list = []
 property_address_list = []
@@ -16,6 +16,17 @@ for property in properties:
     property_url_list.append(property.find("a").get('href'))
     property_address_list.append(property.find("address").text.strip())
 
-print(property_price_list)
-print(property_url_list)
-print(property_address_list)
+# print(property_price_list)
+# print(property_url_list)
+# print(property_address_list)
+
+properties_dict = {}
+
+for index in range(len(property_price_list)):
+    properties_dict[index] = {
+        "Address": property_address_list[index],
+        "Price": property_price_list[index],
+        "URL": property_url_list[index]
+    }
+
+# print(properties_dict)
